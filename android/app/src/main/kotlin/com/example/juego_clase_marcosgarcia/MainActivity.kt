6e09adc2e3699af129dc198ce643cc2928803842
Marcos García <markarules01@gmail.com>
2024-01-12 17:34:19 +0100

    Commit1: Crear ClaseJuego que extiende de FlameGame con componentes manejados por teclado.

---
+++ android/app/src/main/kotlin/com/example/juego_clase_marcosgarcia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.juego_clase_marcosgarcia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/juegos/ClaseJuego.dart
@@ -0,0 +1,7 @@
+import 'package:flame/camera.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+
+class ClaseJuego extends FlameGame with HasKeyboardHandlerComponents{
+    final world = World();
+}
