8d98d0e16cc7814846d3d8e6d31efe1370dd35d3
Marcos García <markarules01@gmail.com>
2024-01-12 18:36:03 +0100

    Commit4: Mapa cargado en el juego y mostrado en la pantalla, nombre de la carpeta cambiado para corregir errores del pubspec.yaml.

--- lib/juegos/ClaseJuego.dart
+++ lib/juegos/ClaseJuego.dart
@@ -1,6 +1,15 @@
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 
 class ClaseJuego extends FlameGame with HasKeyboardHandlerComponents{
     ClaseJuego();
+
+    late TiledComponent mapComponent;
+
+    @override
+    Future<void> onLoad() async {
+        mapComponent = await TiledComponent.load('mapa1.tmx', Vector2.all(32));
+        world.add(mapComponent);
+    }
 }
