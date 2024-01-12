c6393fda7d6896893c3f2d3ed1bd88cb2239aba5
Marcos García <markarules01@gmail.com>
2024-01-12 23:55:56 +0100

    Commit10: ClaseEnemigo y su cuerpo para correspondientes colisiones creados.

---
+++ lib/personajes/ClaseEnemigo.dart
@@ -0,0 +1,21 @@
+import 'package:flame/components.dart';
+import '../juegos/ClaseJuego.dart';
+
+class WaterPlayer extends SpriteAnimationComponent
+    with HasGameRef<ClaseJuego> {
+  WaterPlayer({
+    required super.position,
+  }) : super(size: Vector2.all(64), anchor: Anchor.center);
+
+  @override
+  void onLoad() {
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('water_enemy.png'),
+      SpriteAnimationData.sequenced(
+        amount: 2,
+        textureSize: Vector2.all(16),
+        stepTime: 0.12,
+      ),
+    );
+  }
+}
