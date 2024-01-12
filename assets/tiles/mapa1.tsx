b3008e66c39cf4c3b6d0e39a2a6631652f76950d
Marcos García <markarules01@gmail.com>
2024-01-12 17:52:16 +0100

    Commit3: Añadidos el mapa mediante tiles de Tiled y los personajes mediante imágenes de flutterFlame, añadido además un mapa secundario en caso de error.

---
+++ assets/tiles/mapa1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="mapa1" tilewidth="32" tileheight="32" tilecount="1176" columns="42">
+ <image source="mapa1.png" width="1344" height="896"/>
+</tileset>
--- lib/juegos/ClaseJuego.dart
+++ lib/juegos/ClaseJuego.dart
@@ -1,7 +1,6 @@
-import 'package:flame/camera.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 
 class ClaseJuego extends FlameGame with HasKeyboardHandlerComponents{
-    final world = World();
+    ClaseJuego();
 }
